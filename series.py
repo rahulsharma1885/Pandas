cd25fc372e23d76a394384c57d98dcdada822e7b
Rahul Sharma <rahul.sharma@example.com>
2024-08-05 22:23:40 +0530

    q

--- series.py
+++ series.py
@@ -1,10 +1,11 @@
 import pandas as pd
-# data= [3,4,5,6,8]
-# x= pd.Series(data)
-# print(x)
-
-data= [23, 56, 78, 76, 22, 78]
-y= pd.Series(data)
-z= pd.Series(data[3])
+data= [3,4,5,6,8]
+x= pd.Series(data)
+print(x)
+############################################
+data1= [23, 56, 78, 76, 22, 78]
+y= pd.Series(data1)
+z= pd.Series(data1[3])
 print(y)
 print(z)
+
